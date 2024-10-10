e930db86e068c7f82d479a0d287abd71752afd1b
Mark Wonah <markwonah@gmail.com>
2024-10-10 12:24:48 +0000

    removed whitespace pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,13 +6,13 @@ def canUnlockAll(boxes):
     # Start with box 0 unlocked
     unlocked = set([0])
     keys = set(boxes[0])  # Start with keys from the first box
-    
+
     # Traverse the keys, unlocking boxes
     while keys:
         new_key = keys.pop()
         if new_key < len(boxes) and new_key not in unlocked:
             unlocked.add(new_key)
             keys.update(boxes[new_key])  # Add keys from the newly unlocked box
-    
+
     # Check if all boxes are unlocked
     return len(unlocked) == len(boxes)
